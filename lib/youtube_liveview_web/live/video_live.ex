baee919069b82793837bbc443439a622d82d02ab
Emmanuel Valaquio <v-emmanuel.valaquio@lionbridge.com>
2021-06-15 09:17:26 +0900

    POC for pubsub and handling events

--- lib/youtube_liveview_web/live/video_live.ex
+++ lib/youtube_liveview_web/live/video_live.ex
@@ -1,30 +1,64 @@
 defmodule YoutubeLiveviewWeb.VideoLive do
   use YoutubeLiveviewWeb, :live_view
 
+  @topic "deployments"
+
   @impl true
   def mount(_params, _session, socket) do
-    assigns = [
-      data: "",
-      embedded_link: ""
-    ]
+    YoutubeLiveviewWeb.Endpoint.subscribe(@topic)
+
+    assigns = [data: "", embedded_link: ""]
 
     {:ok, assign(socket, assigns)}
   end
 
   @impl true
   def handle_event("load-video", %{"data" => data}, socket) do
-    assigns = [
-      data: data,
-      embedded_link: create_embedded_link(data)
-    ]
+    assigns = [data: data, embedded_link: create_embedded_link(data)]
 
-    IO.inspect(create_embedded_link(data), label: "embedded_link")
+    YoutubeLiveviewWeb.Endpoint.broadcast_from(self(), @topic, "load-video", assigns)
+
+    {
+      :noreply,
+      socket
+      |> push_event("sample", %{action: "play-video"})
+      |> assign(assigns)
+    }
+  end
+
+  @impl true
+  def handle_info(
+        %{
+          topic: @topic,
+          event: "load-video",
+          payload: assigns
+        },
+        socket
+      ) do
+    {
+      :noreply,
+      socket
+      |> push_event("sample", %{action: "play-video"})
+      |> assign(assigns)
+    }
+  end
+
+  @impl true
+  def handle_info(
+        %{
+          topic: @topic,
+          event: "play-video",
+          payload: assigns
+        },
+        socket
+      ) do
     {:noreply, assign(socket, assigns)}
   end
 
   defp create_embedded_link(data) do
-    [match] = Regex.run(~r/[\d\w]{11}/, data)
-    # IO.inspect(match, label: "match")
-    "https://www.youtube.com/embed/#{match}"
+    case Regex.run(~r/[\d\w]{11}/, data) do
+      [match] -> "https://www.youtube.com/embed/#{match}"
+      nil -> ""
+    end
   end
 end
