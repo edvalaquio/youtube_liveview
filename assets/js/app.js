485fed1ae4483393f83ddb984359c1647716a167
Emmanuel Valaquio <v-emmanuel.valaquio@lionbridge.com>
2021-06-20 00:59:23 +0900

    Handle pause and play events from server; Refactor player and hooks creation

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,31 +19,85 @@ import { LiveSocket } from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
+// Show progress bar on live navigation and form submits
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
+window.addEventListener("phx:page-loading-start", info => topbar.show())
+window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+
 // FIXME: Place in a separate file
 let Hooks = {}
 Hooks.Sample = {
-    mounted() {
-        this.handleEvent(
-            "sample",
-            ({ action }) => {
-                console.log(`Received the following action in the payload - ${action}`)
+    getPlayer() { return this.player },
+    setPlayer() {
+        this.player = new YT.Player("existing-iframe-example");
+
+        this.player.addEventListener("onReady", (_event) => {
+            document.getElementById("existing-iframe-example").style.borderColor = "#FF6D00";
+        });
+
+        this.player.addEventListener("onStateChange", (event) => {
+            const playerStatus = event.data
+            console.log(`player status changed ${playerStatus}`)
+
+            let color;
+            switch (playerStatus) {
+                case -1:
+                    color = "#37474F"; // unstarted = gray
+                    break;
+                case 0:
+                    color = "#FFFF00"; // ended = yellow
+                    break;
+                case 1:
+                    // NOTE: We can use this video state to pause and play videos.
+                    // pros: No need to create custom buttons
+                    // cons: Longer delay in playing videos
+                    // this.pushEvent("play-video", {});
+                    color = "#33691E"; // playing = green
+                    break;
+                case 2:
+                    // NOTE: We can use this video state to pause and play videos.
+                    // pros: No need to create custom buttons
+                    // cons: Longer delay in playing videos
+                    // this.pushEvent("pause-video", {});
+                    color = "#DD2C00"; // paused = red
+                    break;
+                case 3:
+                    color = "#AA00FF"; // buffering = purple
+                    break;
+                case 5:
+                    color = "#FF6DOO"; // video cued = orange
+                    break;
             }
-        )
+            if (color) {
+                document.getElementById("existing-iframe-example").style.borderColor = color;
+            }
+
+        });
+
+        this.player.addEventListener("onError", (event) => {
+            console.error(event.data)
+        });
+    },
+    mounted() {
+        this.setPlayer();
         this.handleEvent(
-            "load-video",
-            () => {
-                console.log("Load-video event called")
+            "client-playback-event",
+            ({ "action": action }) => {
+                console.log(`playback-event event called with action - ${action}}`);
+                switch (action) {
+                    case "play":
+                        this.getPlayer().playVideo();
+                        break;
+                    case "pause":
+                        this.getPlayer().pauseVideo();
+                        break;
+                }
             }
         )
     }
 }
 
 let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks, params: { _csrf_token: csrfToken } })
-
-// Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
@@ -53,39 +107,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
-var player;
-window.onYouTubeIframeAPIReady = () => {
-    console.log("HELLO WORLD")
-    player = new YT.Player('existing-iframe-example', {
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
-        }
-    });
-}
-function onPlayerReady(event) {
-    document.getElementById('existing-iframe-example').style.borderColor = '#FF6D00';
-}
-function changeBorderColor(playerStatus) {
-    var color;
-    if (playerStatus == -1) {
-        color = "#37474F"; // unstarted = gray
-    } else if (playerStatus == 0) {
-        color = "#FFFF00"; // ended = yellow
-    } else if (playerStatus == 1) {
-        color = "#33691E"; // playing = green
-    } else if (playerStatus == 2) {
-        color = "#DD2C00"; // paused = red
-    } else if (playerStatus == 3) {
-        color = "#AA00FF"; // buffering = purple
-    } else if (playerStatus == 5) {
-        color = "#FF6DOO"; // video cued = orange
-    }
-    if (color) {
-        document.getElementById('existing-iframe-example').style.borderColor = color;
-    }
-}
-function onPlayerStateChange(event) {
-    changeBorderColor(event.data);
-}
